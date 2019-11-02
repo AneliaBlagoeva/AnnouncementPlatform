00e044877ca10cab1978731d20aea20ff171f452
Anelia Blagoeva <arblagoeva@gmail.com>
2019-11-02 23:37:07 +0200

    Added APIs to AnnouncementController

--- src/main/java/com/project/AnnouncementPlatform/AnnouncementController.java
+++ src/main/java/com/project/AnnouncementPlatform/AnnouncementController.java
@@ -4,9 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/")
@@ -19,5 +26,27 @@ public class AnnouncementController {
 		public ResponseEntity<List<Announcement>> getAnnouncements() {
 	        return ResponseEntity.ok(anncmntService.findAll());
 	    }
+		
+		@GetMapping("/announcements/{id}")
+		public ResponseEntity<Announcement> getAnnouncementById(@PathVariable int id) {
+			Optional<Announcement> ann=anncmntService.findById(id);
+	        return ResponseEntity.ok(ann.get());
+	    }
+		
+		@PostMapping("/announcements")
+	    public ResponseEntity<Announcement> create(@Valid @RequestBody Announcement ann) {
+	        return ResponseEntity.ok(anncmntService.save(ann));
+	    }
+		
+		@DeleteMapping("/announcements/{id}")
+		    public ResponseEntity<Announcement> delete(@PathVariable int id) {
+		        if (!anncmntService.findById(id).isPresent()) {
+		            ResponseEntity.badRequest().build();
+		        }
+
+		        anncmntService.deleteById(id);
+
+		        return ResponseEntity.ok().build();
+		    }
 }
 
--- src/main/java/com/project/AnnouncementPlatform/AnnouncementService.java
+++ src/main/java/com/project/AnnouncementPlatform/AnnouncementService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AnnouncementService {
@@ -12,5 +13,17 @@ public class AnnouncementService {
 
     public List<Announcement> findAll() {
         return annRespository.findAll();
+    }
+    
+    public Optional<Announcement> findById(int id) {
+        return annRespository.findById(id);
+    }
+    
+    public void deleteById(int id) {
+    	annRespository.deleteById(id);
+    }
+    
+    public Announcement save(Announcement ann) {
+        return annRespository.save(ann);
     }
 }
