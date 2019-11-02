360b8999c1faa7ef4a379d79205ba3e52ecbae5d
Anelia Blagoeva <arblagoeva@gmail.com>
2019-11-02 19:17:44 +0200

    Initial version

---
+++ src/main/java/com/project/AnnouncementPlatform/AnnouncementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.project.AnnouncementPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnouncementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/AnnouncementPlatform/AnnouncementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AnnouncementPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
