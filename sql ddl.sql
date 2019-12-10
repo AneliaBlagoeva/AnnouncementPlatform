53dcca0d14a078ff5da007f367b581a25cccf9ff
Anelia Blagoeva <arblagoeva@gmail.com>
2019-12-10 22:56:55 +0200

    Changed DDL for announcement id Auto increment

--- sql ddl.sql
+++ sql ddl.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `announcement` (
-  `announcement_id` int(11) NOT NULL,
+  `announcement_id` int(11) NOT NULL AUTO_INCREMENT,
   `anncmnt_name` varchar(45) NOT NULL,
   `status_id` int(11) NOT NULL,
   `anncmnt_category_id` int(11) NOT NULL,
@@ -12,21 +12,21 @@ CREATE TABLE `announcement` (
   KEY `statusId_idx` (`status_id`),
   KEY `email_idx` (`user_email`),
   CONSTRAINT `category_id` FOREIGN KEY (`anncmnt_category_id`) REFERENCES `announcementcategory` (`category_id`),
-  CONSTRAINT `email` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`),
+  CONSTRAINT `email` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `announcementstatus` (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `announcementcategory` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(45) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `announcementstatus` (
   `status_id` int(11) NOT NULL,
   `status_name` varchar(45) NOT NULL,
   PRIMARY KEY (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `job` (
   `anncmnt_id` int(11) NOT NULL,
@@ -36,14 +36,14 @@ CREATE TABLE `job` (
   `experience` int(11) DEFAULT NULL,
   `benefits` varchar(3000) DEFAULT NULL,
   `requirements` varchar(5000) DEFAULT NULL,
-  PRIMARY KEY (`anncmnt_id`),
+  KEY `jobId_idx` (`anncmnt_id`),
   CONSTRAINT `jobId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `role` (
   `description` varchar(45) NOT NULL,
   PRIMARY KEY (`description`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `scholarship` (
   `anncmnt_id` int(11) NOT NULL,
@@ -56,7 +56,7 @@ CREATE TABLE `scholarship` (
   `degree_allowed` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`anncmnt_id`),
   CONSTRAINT `scholarshipId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `user` (
   `first_name` varchar(45) NOT NULL,
@@ -70,8 +70,8 @@ CREATE TABLE `user` (
   `password` varchar(256) NOT NULL,
   PRIMARY KEY (`email`),
   KEY `role_idx` (`role`),
-  CONSTRAINT `role` FOREIGN KEY (`role`) REFERENCES `role` (`description`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `roleFk` FOREIGN KEY (`role`) REFERENCES `role` (`description`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `userjobattributes` (
   `skills` varchar(1024) NOT NULL,
@@ -80,7 +80,7 @@ CREATE TABLE `userjobattributes` (
   PRIMARY KEY (`user_email`),
   KEY `email_idx` (`user_email`),
   CONSTRAINT `jobUserEmail` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `userschoralshipattributes` (
   `user_email` varchar(45) NOT NULL,
@@ -91,22 +91,18 @@ CREATE TABLE `userschoralshipattributes` (
   `year` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_email`),
   CONSTRAINT `userEmail` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `uservolunteerattributes` (
   `user_email` varchar(45) NOT NULL,
   PRIMARY KEY (`user_email`),
   CONSTRAINT `fkEmail` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `version` (
-  `version` decimal(1,0) NOT NULL,
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `voluntariness` (
   `anncmnt_id` int(11) NOT NULL,
   `anncmnt_organization` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`anncmnt_id`),
-  CONSTRAINT `vountarinessId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `voluntarinessId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SELECT * FROM announcementplatform.job;
