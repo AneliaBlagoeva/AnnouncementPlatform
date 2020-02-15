f82fa0047bd16d49b2be90a21949eea1b1fcec74
Anelia Blagoeva <arblagoeva@gmail.com>
2020-02-15 20:39:17 +0200

    DDL changes

--- sql ddl.sql
+++ sql ddl.sql
@@ -1,24 +1,22 @@
 CREATE DATABASE `announcementplatform` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-CREATE TABLE `role` (
-  `description` varchar(45) NOT NULL,
-  PRIMARY KEY (`description`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `user` (
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `age` smallint(99) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `phone` char(10) DEFAULT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  `role` varchar(45) DEFAULT NULL,
-  `password` varchar(256) NOT NULL,
-  PRIMARY KEY (`email`),
-  KEY `role_idx` (`role`),
-  CONSTRAINT `roleFk` FOREIGN KEY (`role`) REFERENCES `role` (`description`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `announcement` (
+  `announcement_id` int(11) NOT NULL AUTO_INCREMENT,
+  `anncmnt_name` varchar(45) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `anncmnt_category_id` int(11) NOT NULL,
+  `description` varchar(5000) DEFAULT NULL,
+  `url` varchar(45) NOT NULL,
+  `date_validity` datetime DEFAULT NULL,
+  `user_email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`announcement_id`),
+  KEY `categoryId_idx` (`anncmnt_category_id`),
+  KEY `statusId_idx` (`status_id`),
+  KEY `email_idx` (`user_email`),
+  CONSTRAINT `category_id` FOREIGN KEY (`anncmnt_category_id`) REFERENCES `announcementcategory` (`category_id`),
+  CONSTRAINT `email` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `announcementstatus` (`status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `announcementcategory` (
   `category_id` int(11) NOT NULL,
@@ -32,34 +30,28 @@ CREATE TABLE `announcementstatus` (
   PRIMARY KEY (`status_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `announcement` (
-  `announcement_id` int(11) NOT NULL AUTO_INCREMENT,
-  `anncmnt_name` varchar(45) NOT NULL,
-  `status_id` int(11) NOT NULL,
-  `anncmnt_category_id` int(11) NOT NULL,
-  `description` varchar(5000) DEFAULT NULL,
-  `url` varchar(45) NOT NULL,
-  `date_created` datetime DEFAULT NULL,
-  `user_email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`announcement_id`),
-  KEY `categoryId_idx` (`anncmnt_category_id`),
-  KEY `statusId_idx` (`status_id`),
-  KEY `email_idx` (`user_email`),
-  CONSTRAINT `category_id` FOREIGN KEY (`anncmnt_category_id`) REFERENCES `announcementcategory` (`category_id`),
-  CONSTRAINT `email` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `announcementstatus` (`status_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+CREATE TABLE `degree` (
+  `iddegree` int(11) NOT NULL,
+  `degree_description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`iddegree`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `job` (
   `anncmnt_id` int(11) NOT NULL,
   `anncmnt_organization` varchar(45) DEFAULT NULL,
   `candidate_responsibility` varchar(2048) DEFAULT NULL,
-  `salary` int(11) DEFAULT NULL,
+  `salary` varchar(45) DEFAULT NULL,
   `experience` int(11) DEFAULT NULL,
   `benefits` varchar(3000) DEFAULT NULL,
   `requirements` varchar(5000) DEFAULT NULL,
   KEY `jobId_idx` (`anncmnt_id`),
   CONSTRAINT `jobId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `role` (
+  `description` varchar(45) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `scholarship` (
@@ -70,16 +62,25 @@ CREATE TABLE `scholarship` (
   `program_allowed` varchar(2048) DEFAULT NULL,
   `university_allowed` varchar(2048) DEFAULT NULL,
   `year_allowed` int(11) DEFAULT NULL,
-  `degree_allowed` varchar(45) DEFAULT NULL,
+  `degree_allowed` int(11) DEFAULT NULL,
   PRIMARY KEY (`anncmnt_id`),
+  KEY `degreeId_idx` (`degree_allowed`),
+  CONSTRAINT `degreeId` FOREIGN KEY (`degree_allowed`) REFERENCES `degree` (`iddegree`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `scholarshipId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `voluntariness` (
-  `anncmnt_id` int(11) NOT NULL,
-  `anncmnt_organization` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`anncmnt_id`),
-  CONSTRAINT `voluntarinessId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `user` (
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `phone` char(10) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
+  `password` varchar(256) NOT NULL,
+  PRIMARY KEY (`email`),
+  KEY `role_idx` (`role`),
+  CONSTRAINT `roleId` FOREIGN KEY (`role`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `userjobattributes` (
@@ -94,11 +95,13 @@ CREATE TABLE `userjobattributes` (
 CREATE TABLE `userschoralshipattributes` (
   `user_email` varchar(45) NOT NULL,
   `grade` decimal(3,2) DEFAULT NULL,
-  `degree` varchar(45) DEFAULT NULL,
+  `degree` int(11) DEFAULT NULL,
   `university` varchar(45) DEFAULT NULL,
   `program` varchar(45) DEFAULT NULL,
   `year` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_email`),
+  KEY `degreeAnnId_idx` (`degree`),
+  CONSTRAINT `degreeAnnId` FOREIGN KEY (`degree`) REFERENCES `degree` (`iddegree`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `userEmail` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -107,6 +110,14 @@ CREATE TABLE `uservolunteerattributes` (
   PRIMARY KEY (`user_email`),
   CONSTRAINT `fkEmail` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `voluntariness` (
+  `anncmnt_id` int(11) NOT NULL,
+  `anncmnt_organization` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`anncmnt_id`),
+  CONSTRAINT `voluntarinessId` FOREIGN KEY (`anncmnt_id`) REFERENCES `announcement` (`announcement_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 INSERT INTO `announcementplatform`.`announcementcategory`
 (`category_id`,`category_name`) VALUES
